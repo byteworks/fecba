222c52e3ef57bb58a7e4bf109a8f23814d61f6b5
Byteworks <info@bworks.org>
2011-01-18 21:38:06 -0600

    added log in date field redirect to list page after logging in

--- fecba.rb
+++ fecba.rb
@@ -8,35 +8,35 @@ DB = Sequel.connect('sqlite://fecba.db')
 DB.create_table? :logins do
     primary_key :id
     String :username
-    # DateTime :login_date
+    DateTime :login_date
     # DateTime :logout_date
 end
 
 
 get '/' do
-	if rand(2)== 1
-		@picture="fecba_logo_blast.jpg"
-	else
-		@picture="fecba_logo_rattle.png"
-	end
-	erb :index
+    if rand(2)== 1
+        @picture="fecba_logo_blast.jpg"
+    else
+        @picture="fecba_logo_rattle.png"
+    end
+    erb :index
 end
 
 get '/list' do
-    
     records = ""
     @login_list = DB.fetch( "SELECT * FROM logins" )
     erb :list
-        
 end
 
 post '/go' do
     words=params["command"].split(" ")
 
     if words[0]=="login"
-        DB.run("INSERT INTO logins (username) VALUES ('#{words[1]}');")
+        sql = "INSERT INTO logins (username, login_date) " +
+              "VALUES ('#{words[1]}', datetime('now'));"
+        DB.run(sql)
     end
-    words.inspect()
+    redirect '/list'
 end
 
 enable :inline_templates
@@ -47,7 +47,7 @@ __END__
 <body>
 <ul>
 <% for login in @login_list %>
-    <li><%= login[:username] %>&#124;</li>
+    <li><%= login[:username] %>&#124;<%= login[:login_date]%></li>
 <% end %>
 </ul>
 </body>
